987c384446984cc3887f448c8278dedba363dcd3
kalaiyarasiS <kalaiyarasiks22@gmail.com>
2016-09-12 19:59:45 +0530

    Update and rename kruskal9965.java to Kalai.java

---
+++ Kalai.java
@@ -0,0 +1,40 @@
+package problem;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author R.AADHAVAN
+ */
+public class Kalai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      String s1="hello";
+for(int  i=0;i<=s1.length();i++)
+{
+    char e=s1.charAt(i);
+    if((e== 'A')||(e=='E')||(e=='I')||(e=='O')||(e=='U')){
+        {
+String h=s1.substring(0, i);
+
+String g=s1.substring(i+ i);
+s1=h+"Z"+g;
+
+        
+
+
+
+}}
+    
+    
+    
+}
+    System.out.println("The result is="+s1);
+
+
+
+                
+    }}
